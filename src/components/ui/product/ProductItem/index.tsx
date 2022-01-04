dbd7b884b1a654d0b5c5b87582d6afc136c7fa21
taiji <k.taiji1108@gmail.com>
2022-01-04 17:57:14 +0900

    ADD: ボタン追加

--- src/components/ui/product/ProductItem/index.tsx
+++ src/components/ui/product/ProductItem/index.tsx
@@ -1,4 +1,5 @@
-import { Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react'
+import { Box, Button, HStack, Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react'
+import React from 'react'
 
 import { Product } from '@/types/product'
 
@@ -6,11 +7,29 @@ type ProductItemPropsType = Product
 
 export const ProductItem = ({ id, productName, price, description }: ProductItemPropsType) => {
   console.log(id)
+
+  const onClickAdd = () => {
+    console.log('add')
+  }
+  const onClickDelete = () => {
+    console.log('delete')
+  }
+
   return (
-    <Stat>
-      <StatLabel>{productName}</StatLabel>
-      <StatNumber>¥{price}</StatNumber>
-      <StatHelpText>{description}</StatHelpText>
-    </Stat>
+    <Box borderRadius={'md'} borderWidth={2} px={5} py={4} w={'sm'}>
+      <Stat>
+        <StatLabel>{productName}</StatLabel>
+        <StatNumber>¥{price}</StatNumber>
+        <StatHelpText>{description}</StatHelpText>
+      </Stat>
+      <HStack spacing="3">
+        <Button bg="blue.300" onClick={onClickAdd}>
+          追加
+        </Button>
+        <Button bg="red.300" onClick={onClickDelete}>
+          削除
+        </Button>
+      </HStack>
+    </Box>
   )
 }
