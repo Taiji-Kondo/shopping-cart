4847b4e24fdee3c560633c6227faf6882e460834
taiji <k.taiji1108@gmail.com>
2022-01-04 17:34:25 +0900

    CREATE: 商品一覧のデータ作成

---
+++ src/constants/product/productList.ts
@@ -0,0 +1,22 @@
+import { Product } from '@/types/product'
+
+export const productList: Product[] = [
+  {
+    id: 1,
+    productName: 'りんご',
+    price: 100,
+    description: '赤いしいりんごです',
+  },
+  {
+    id: 2,
+    productName: 'みかん',
+    price: 80,
+    description: '甘いみかんです',
+  },
+  {
+    id: 3,
+    productName: 'バナナ',
+    price: 200,
+    description: '6本入りのバナナです',
+  },
+]
