2615fa3fa9572c444f2b4332819b24409e31e1e1
taiji <k.taiji1108@gmail.com>
2022-01-04 17:37:10 +0900

    ADD: 商品のコンポーネント組み込み

--- src/components/pages/products/Contents.tsx
+++ src/components/pages/products/Contents.tsx
@@ -1,3 +1,14 @@
+import { Heading } from '@chakra-ui/react'
+
+import { ProductList } from '@/components/ui/product/ProductItem'
+
+import { productList } from '@/constants/product/productList'
+
 export const ProductsContents = () => {
-  return <></>
+  return (
+    <>
+      <Heading>商品一覧</Heading>
+      <ProductList {...productList} />
+    </>
+  )
 }
