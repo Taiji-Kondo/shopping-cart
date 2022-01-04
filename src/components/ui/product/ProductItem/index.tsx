4e836e0d573fa4ca634da935f1c6560f05090e18
taiji <k.taiji1108@gmail.com>
2022-01-04 23:37:12 +0900

    CREATE: cart操作の処理をカスタムフック切り出し

--- src/components/ui/product/ProductItem/index.tsx
+++ src/components/ui/product/ProductItem/index.tsx
@@ -1,60 +1,14 @@
 import { Box, Button, HStack, Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react'
 import React from 'react'
-import { useRecoilState } from 'recoil'
-
-import { productList } from '@/constants/product/productList'
 
 import { Product } from '@/types/product'
 
-import { cartsState } from '@/stores/atoms/cartState'
+import { useCart } from '@/hooks'
 
 type ProductItemPropsType = Product
 
 export const ProductItem = ({ id, productName, price, description }: ProductItemPropsType) => {
-  const [cart, setCart] = useRecoilState(cartsState)
-
-  // 商品一覧から指定された商品IDの商品を取り出す
-  const findProductItem = (productId: number) =>
-    productList.find((productItem) => productItem.id === productId)
-
-  // カート一覧からしていされた商品IDの商品を取り出す
-  const findCartItem = (productId: number) => cart.find((cartItem) => cartItem.id === productId)
-
-  const addCartItem = (productId: number) => {
-    const selectedProduct = findProductItem(productId)
-    if (!selectedProduct) return
-
-    // カート内に追加しようとしている商品が既にあるかどうか
-    const hasSelectedProductInCart = findCartItem(productId)
-
-    if (!hasSelectedProductInCart) {
-      // まだカート内になければカウントを1にしてそのまま追加
-      setCart((prev) => {
-        return [...prev, { ...selectedProduct, count: 1 }]
-      })
-    } else {
-      // すでにカートにある場合はカウントを1増やす
-      setCart((prev) => {
-        return prev.map((cartItem) => {
-          if (cartItem.id !== productId) return cartItem
-
-          return { ...cartItem, count: cartItem.count + 1 }
-        })
-      })
-    }
-  }
-
-  const removeCartItem = (productId: number) => {
-    const _cart = cart.map((cartItem) => {
-      if (cartItem.id !== productId) return cartItem
-      if (cartItem.count === 0) return cartItem
-
-      return { ...cartItem, count: cartItem.count - 1 }
-    })
-
-    // countが0の商品を取り除いたものを返す
-    setCart(() => _cart.filter((cartItem) => cartItem.count !== 0))
-  }
+  const { addCartItem, removeCartItem } = useCart()
 
   const onClickAdd = (productId: number) => {
     addCartItem(productId)
---
+++ src/hooks/cart/useCart.ts
@@ -0,0 +1,54 @@
+import { useRecoilState } from 'recoil'
+
+import { productList } from '@/constants/product/productList'
+
+import { cartsState } from '@/stores/atoms/cartState'
+
+export const useCart = () => {
+  const [cart, setCart] = useRecoilState(cartsState)
+
+  // 商品一覧から指定された商品IDの商品を取り出す
+  const _findProductItem = (productId: number) =>
+    productList.find((productItem) => productItem.id === productId)
+
+  // カート一覧からしていされた商品IDの商品を取り出す
+  const _findCartItem = (productId: number) => cart.find((cartItem) => cartItem.id === productId)
+
+  const addCartItem = (productId: number) => {
+    const selectedProduct = _findProductItem(productId)
+    if (!selectedProduct) return
+
+    // カート内に追加しようとしている商品が既にあるかどうか
+    const hasSelectedProductInCart = _findCartItem(productId)
+
+    if (!hasSelectedProductInCart) {
+      // まだカート内になければカウントを1にしてそのまま追加
+      setCart((prev) => {
+        return [...prev, { ...selectedProduct, count: 1 }]
+      })
+    } else {
+      // すでにカートにある場合はカウントを1増やす
+      setCart((prev) => {
+        return prev.map((cartItem) => {
+          if (cartItem.id !== productId) return cartItem
+
+          return { ...cartItem, count: cartItem.count + 1 }
+        })
+      })
+    }
+  }
+
+  const removeCartItem = (productId: number) => {
+    const _cart = cart.map((cartItem) => {
+      if (cartItem.id !== productId) return cartItem
+      if (cartItem.count === 0) return cartItem
+
+      return { ...cartItem, count: cartItem.count - 1 }
+    })
+
+    // countが0の商品を取り除いたものを返す
+    setCart(() => _cart.filter((cartItem) => cartItem.count !== 0))
+  }
+
+  return { addCartItem, removeCartItem }
+}
