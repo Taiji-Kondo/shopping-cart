c0e781b4ab3a6e20f8d7889ccef6644629f49d5d
taiji <k.taiji1108@gmail.com>
2022-01-09 00:44:11 +0900

    CREATE: カートの合計を表示

--- src/components/ui/cart/SideCart.tsx
+++ src/components/ui/cart/SideCart.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Text } from '@chakra-ui/react'
+import { Box, HStack, StackDivider, Text, VStack } from '@chakra-ui/react'
 
 import { cartStateType } from '@/stores/atoms/cartState'
 
@@ -7,14 +7,21 @@ type SideCartPropsType = {
 }
 
 export const SideCart = ({ cart }: SideCartPropsType) => {
+  const totalPrice = cart.reduce((prev, cartItem) => prev + cartItem.price * cartItem.count, 0)
+
   return (
-    <Box px={6} borderLeftWidth={2}>
-      {cart.map((cartItem) => (
-        <HStack spacing={2} key={cartItem.id}>
-          <Text fontWeight={'bold'}>{cartItem.productName}</Text>
-          <Text>×{cartItem.count}</Text>
-        </HStack>
-      ))}
+    <Box px={6} borderLeftWidth={2} w="100%">
+      <VStack align="stretch" spacing={2} divider={<StackDivider borderColor="gray" />}>
+        <Box>
+          {cart.map((cartItem) => (
+            <HStack spacing={2} key={cartItem.id}>
+              <Text fontWeight={'bold'}>{cartItem.productName}</Text>
+              <Text>×{cartItem.count}</Text>
+            </HStack>
+          ))}
+        </Box>
+        <Text fontWeight="bold">¥{totalPrice}</Text>
+      </VStack>
     </Box>
   )
 }
